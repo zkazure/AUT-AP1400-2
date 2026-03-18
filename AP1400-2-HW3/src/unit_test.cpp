cb4f3593a7454c6fa71bd869977969950307eec6
Kazure Zheng <kazure@debian>
2026-03-18 19:08:34 +0800

    pass test9

--- AP1400-2-HW3/src/unit_test.cpp
+++ AP1400-2-HW3/src/unit_test.cpp
@@ -101,7 +101,7 @@ TEST(HW3Test, TEST8) {
     EXPECT_EQ(bst.get_root()->left->right->value, 15);
     EXPECT_EQ(bst.get_root()->right->right->value, 53);
 }
-/*
+
 TEST(HW3Test, TEST9) {
     BST bst{};
     bst.add_node(25);
@@ -112,14 +112,14 @@ TEST(HW3Test, TEST9) {
     EXPECT_FALSE(bst.add_node(50));
 
     std::vector<int> values;
-    bst.bfs([&values](BST::Node*& node){values.push_back(node->value);});
+    bst.bfs([&values](BST::Node *&node) { values.push_back(node->value); });
     EXPECT_EQ(values.size(), 4);
     EXPECT_TRUE(std::find(values.begin(), values.end(), 25) != values.end());
     EXPECT_TRUE(std::find(values.begin(), values.end(), 10) != values.end());
     EXPECT_TRUE(std::find(values.begin(), values.end(), 50) != values.end());
     EXPECT_TRUE(std::find(values.begin(), values.end(), 53) != values.end());
 }
-
+/*
 TEST(HW3Test, TEST10) {
     BST bst{};
     bst.add_node(25);
