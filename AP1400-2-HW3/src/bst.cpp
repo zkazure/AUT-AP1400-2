b4b2c856b62c1788ca617f69d44bf6e09a76da00
Kazure Zheng <kazure@debian>
2026-03-19 20:37:51 +0800

    some modify

--- AP1400-2-HW3/src/bst.cpp
+++ AP1400-2-HW3/src/bst.cpp
@@ -6,12 +6,12 @@
 #include <string>
 #include <vector>
 
-void BST::bfs(std::function<void(BST::Node *&node)> func) const {
-    std::queue<BST::Node *> que;
+void BST::bfs(std::function<void(Node *&node)> func) const {
+    std::queue<Node *> que;
     if (root)
         que.push(root);
     while (que.empty() == false) {
-        BST::Node *node = que.front();
+        Node *node = que.front();
         que.pop();
         func(node);
         if (node->left)
@@ -34,33 +34,33 @@ BST::Node *BST::clone(const Node *other) const {
 BST::BST(const BST &other) { root = clone(other.root); }
 
 BST::~BST() {
-    std::vector<BST::Node *> nodes;
-    bfs([&nodes](BST::Node *&node) { nodes.push_back(node); });
+    std::vector<Node *> nodes;
+    bfs([&nodes](Node *&node) { nodes.push_back(node); });
     for (auto node : nodes)
         delete node;
 }
 
 size_t BST::length() const {
     size_t len = 0;
-    bfs([&len](BST::Node *&node) { len++; });
+    bfs([&len](Node *&node) { len++; });
     return len;
 }
 
 bool BST::add_node(int value) {
     if (!root) {
-        root = new BST::Node(value, nullptr, nullptr);
+        root = new Node(value, nullptr, nullptr);
         return true;
     }
 
-    BST::Node *curr = root;
-    BST::Node *prev = nullptr;
+    Node *curr = root;
+    Node *prev = nullptr;
     while (curr) {
         if (value == *curr)
             return false;
         prev = curr;
         curr = (value < *curr) ? curr->left : curr->right;
     }
-    BST::Node *node = new BST::Node(value, nullptr, nullptr);
+    Node *node = new BST::Node(value, nullptr, nullptr);
     if (value < *prev)
         prev->left = node;
     else
@@ -76,8 +76,8 @@ BST::Node **BST::find_node(int value) {
     if (value == *root)
         return &root;
 
-    BST::Node *curr = root;
-    BST::Node *prev = nullptr;
+    Node *curr = root;
+    Node *prev = nullptr;
     while (curr) {
         prev = curr;
         curr = (value < *curr) ? curr->left : curr->right;
@@ -195,13 +195,13 @@ bool BST::delete_node(int value) {
 }
 
 BST &BST::operator++() {
-    bfs([](BST::Node *&node) { node->value += 1; });
+    bfs([](Node *&node) { node->value += 1; });
     return *this;
 }
 
 BST BST::operator++(int) {
     BST tmp = *this;
-    bfs([](BST::Node *&node) { node->value += 1; });
+    bfs([](Node *&node) { node->value += 1; });
     return tmp;
 }
 
