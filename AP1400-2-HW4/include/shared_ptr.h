09d23c2d5a6327e1b5c39fdfa5b44f34ef41e160
Kazure Zheng <kazure@debian>
2026-03-26 09:37:27 +0800

    some modify

--- AP1400-2-HW4/include/shared_ptr.h
+++ AP1400-2-HW4/include/shared_ptr.h
@@ -1,44 +1,6 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
-template <typename T>
-class UniquePtr {
-private:
-    T* _p;
-
-public:
-    UniquePtr() = default;
-    UniquePtr(T value) { _p = new T(value); }
-    UniquePtr(T* pointer) { _p = pointer; }
-
-    T* get() { return _p; }
-    void reset() {
-        delete _p;
-        _p = nullptr;
-    }
-    void reset(T* other) {
-        delete _p;
-        _p = other;
-    }
-
-    operator bool() const { return _p != nullptr; }
-    T &operator*() {
-        return *_p;
-    }
-    T *operator->() {
-        return _p;
-    }
-};
-
-template<typename T>
-UniquePtr<T> make_unique(T value) {
-    return UniquePtr<T>(value);
-}
-
-template<typename T>
-UniquePtr<T> make_unique(T* pointer) {
-    return UniquePtr<T>(pointer);
-}
 
 
 #endif //SHARED_PTR
--- AP1400-2-HW4/include/unique_ptr.h
+++ AP1400-2-HW4/include/unique_ptr.h
@@ -1,14 +1,43 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
 
+template <typename T>
+class UniquePtr {
+private:
+    T* _p;
 
+public:
+    UniquePtr() = default;
+    UniquePtr(T value) { _p = new T(value); }
+    UniquePtr(T* pointer) { _p = pointer; }
 
+    T* get() { return _p; }
+    void reset() {
+        delete _p;
+        _p = nullptr;
+    }
+    void reset(T* other) {
+        delete _p;
+        _p = other;
+    }
 
+    operator bool() const { return _p != nullptr; }
+    T &operator*() {
+        return *_p;
+    }
+    T *operator->() {
+        return _p;
+    }
+};
 
+template<typename T>
+UniquePtr<T> make_unique(T value) {
+    return UniquePtr<T>(value);
+}
 
-
-
-
-
+template<typename T>
+UniquePtr<T> make_unique(T* pointer) {
+    return UniquePtr<T>(pointer);
+}
 
 #endif //UNIQUE_PTR
