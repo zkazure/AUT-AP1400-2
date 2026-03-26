9100dc313fb922a6620d51e3b7e2940d6043a30a
Kazure Zheng <kazure@debian>
2026-03-26 09:27:50 +0800

    pass test5

--- AP1400-2-HW4/include/shared_ptr.h
+++ AP1400-2-HW4/include/shared_ptr.h
@@ -12,10 +12,14 @@ public:
     UniquePtr(T* pointer) { _p = pointer; };
 
     T* get() { return _p; };
-
     T &operator*() {
         return *(this->get());
     }
+    T *operator->() {
+        return this->get();
+    }
+
+
 };
 
 template<typename T>
--- AP1400-2-HW4/src/unit_test.cpp
+++ AP1400-2-HW4/src/unit_test.cpp
@@ -42,7 +42,7 @@ TEST(HW4Test, TEST4) {
     UniquePtr<std::vector<int>> ptr3{new std::vector<int>{1, 2, 3, 4, 5}};
     EXPECT_EQ((*ptr3)[1], 2);
 }
-/*
+
 TEST(HW4Test, TEST5) {
     UniquePtr<std::string> ptr1{new std::string{"hello world!"}};
     EXPECT_EQ(ptr1->length(), 12);
@@ -50,7 +50,7 @@ TEST(HW4Test, TEST5) {
     UniquePtr<std::vector<int>> ptr3{new std::vector<int>{1, 2, 3, 4, 5}};
     EXPECT_FALSE(ptr3->empty());
 }
-
+/*
 TEST(HW4Test, TEST6) {
     UniquePtr<std::string> ptr{new std::string{"hello world!"}};
     EXPECT_EQ(ptr->length(), 12);
